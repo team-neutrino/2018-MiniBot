6902c919016b503bab1fa0f042b4e9003058aff9
JoelNeppel <JoelNeppel@adsl-76-203-56-9.dsl.ltrkar.sbcglobal.net>
2018-09-29 20:18:48 -0500

    Initial push

---
+++ 2018-MiniBot/src/org/usfirst/frc/team3928/robot/Drive.java
@@ -0,0 +1,35 @@
+package org.usfirst.frc.team3928.robot;
+
+import edu.wpi.first.wpilibj.Talon;
+
+public class Drive 
+{
+	private Talon left1;
+	private Talon left2;
+	private Talon right1;
+	private Talon right2;
+	
+	public Drive()
+	{
+		
+		right1 = new Talon(1);
+		right2 = new Talon(2);
+		left1 = new Talon(3);
+		left2 = new Talon(5);
+		
+	}
+	
+	public void setLeft(double motorPower)
+	{
+		left1.set(motorPower);
+		left2.set(motorPower);
+	}
+	
+	public void setRight(double motorPower)
+	{
+		right1.set(motorPower);
+		right2.set(motorPower);
+	}
+	
+	
+}
